450fd37a0bd659ec8d8d582bd91c9db6ca60b6aa
Conall O Maolain <conallomaolain1@gmail.com>
2024-06-04 13:47:28 +0200

    add edit function

--- pages/api/places/[id]/index.js
+++ pages/api/places/[id]/index.js
@@ -16,4 +16,12 @@ export default async function handler(request, response) {
     }
     response.status(200).json({ place: places });
   }
+
+  if (request.method === "PATCH") {
+    await Places.findByIdAndUpdate(id, {
+      $set: request.body,
+    });
+
+    response.status(200).json({ status: `Place ${id} updated!` });
+  }
 }
