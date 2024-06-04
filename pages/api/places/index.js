8bf6cc324c64191a10aaa8ff85bfbbba95dc99a9
Camillo Hochberg <camillo.h@hotmail.de>
2024-06-04 13:48:33 +0200

    add create function

--- pages/api/places/index.js
+++ pages/api/places/index.js
@@ -5,9 +5,21 @@ export default async function handler(request, response) {
   await dbConnect();
   if (request.method === "GET") {
     const places = await Places.find();
+
     if (!places) {
       return response.status(404).json({ status: "Not Found" });
     }
     response.status(200).json(places);
+  }
+  if (request.method === "POST") {
+    try {
+      const placeData = request.body;
+      await Places.create(placeData);
+
+      response.status(201).json({ status: "Place created" });
+    } catch (error) {
+      console.log(error);
+      response.status(400).json({ error: error.message });
+    }
   }
 }
