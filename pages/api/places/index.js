d01868a0eea024081ecffdf742b7ecef6ef9449f
Conall O Maolain <conallomaolain1@gmail.com>
2024-06-04 10:27:20 +0200

    read from database

--- pages/api/places/index.js
+++ pages/api/places/index.js
@@ -1,5 +1,13 @@
-import { db_places } from "../../../lib/db_places";
+import Places from "../../../db/models/Places.js";
+import dbConnect from "../../../db/dbConnect.js";
 
-export default function handler(request, response) {
-  return response.status(200).json(db_places);
+export default async function handler(request, response) {
+  await dbConnect();
+  if (request.method === "GET") {
+    const places = await Places.find();
+    if (!places) {
+      return response.status(404).json({ status: "Not Found" });
+    }
+    response.status(200).json(places);
+  }
 }
