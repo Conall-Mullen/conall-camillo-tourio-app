bb4b7fcee729cdf2ebc90e6c641bd7fa759968c0
Conall O Maolain <conallomaolain1@gmail.com>
2024-06-04 10:40:02 +0200

    write GET request for each page

--- pages/api/places/[id]/index.js
+++ pages/api/places/[id]/index.js
@@ -1,23 +1,19 @@
-import { db_places } from "../../../../lib/db_places";
-import { db_comments } from "../../../../lib/db_comments";
+import Places from "../../../../db/models/Places.js";
+import dbConnect from "../../../../db/dbConnect.js";
 
-export default function handler(request, response) {
+export default async function handler(request, response) {
+  await dbConnect();
   const { id } = request.query;
 
   if (!id) {
     return;
   }
 
-  const place = db_places.find((place) => place._id.$oid === id);
-  const comment = place?.comments;
-  const allCommentIds = comment?.map((comment) => comment.$oid) || [];
-  const comments = db_comments.filter((comment) =>
-    allCommentIds.includes(comment._id.$oid)
-  );
-
-  if (!place) {
-    return response.status(404).json({ status: "Not found" });
+  if (request.method === "GET") {
+    const places = await Places.findById(id);
+    if (!places) {
+      return response.status(404).json({ status: "Not Found" });
+    }
+    response.status(200).json({ place: places });
   }
-
-  response.status(200).json({ place: place, comments: comments });
 }
